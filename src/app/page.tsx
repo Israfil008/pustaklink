7a0928d1e7a3362358c662ab8b385526920c9728
Israfil008 <israfilmiya619@gmail.com>
2025-06-24 13:26:19 +0545

    Fix 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="text-5xl font-bold text-center mb-6">PustakLink</h1>
 
       <p className="text-2xl italic text-gray-600 max-w-xl text-center mb-6">
-        "You always buy a book by judging its cover."
+        &quot;You always buy a book by judging its cover.&quot;
       </p>
 
       <button className="px-6 py-3 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition">
