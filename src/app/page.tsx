57aaae76f824b0991254392b243111429f6e0999
Israfil008 <israfilmiya619@gmail.com>
2025-06-29 14:12:00 +0545

    new begin 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ export default function HomePage() {
     <div className="min-h-screen bg-background text-gray-100 px-6 py-12 text-center">
       <h1 className="text-4xl font-heading mb-4">📚 PustakLink</h1>
       <p className="text-lg font-body text-gray-300 italic">
-        "You always buy a book by judging its cover."
-      </p>
+  &quot;You always buy a book by judging its cover.&quot;
+</p>
+
 
       <div className="mt-10">
         <button className="bg-primary text-black px-6 py-3 rounded-md text-lg font-medium hover:bg-emerald-600 transition">
